deb491b2b6d61ca7ec380a86612ead14eb5d8961
sean-code <john.nganga@student.moringaschool.com>
2023-02-18 21:49:43 +0300

    Create a postgresql database, add attributes, and then run migrations

--- db/migrate/20230218182150_create_users.rb
+++ db/migrate/20230218182150_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.text :username
+      t.text :password_digest
+      t.text :bio
 
       t.timestamps
     end
--- db/migrate/20230218182205_create_notes.rb
+++ db/migrate/20230218182205_create_notes.rb
@@ -1,6 +1,9 @@
 class CreateNotes < ActiveRecord::Migration[7.0]
   def change
     create_table :notes do |t|
+      t.text :title
+      t.text :content
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
