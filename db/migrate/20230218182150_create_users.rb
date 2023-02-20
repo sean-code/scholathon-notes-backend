3a6ad7ee0b82b8fa6f38c8c87b3e04999f24e14a
sean-code <john.nganga@student.moringaschool.com>
2023-02-20 20:25:43 +0300

    Update attribute for password

--- db/migrate/20230218182150_create_users.rb
+++ db/migrate/20230218182150_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.text :username
-      t.text :password_digest
       t.text :bio
+      t.text :password
 
       t.timestamps
     end
