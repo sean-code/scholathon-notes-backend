1cac26867d58378862f704bff67f973c5399bb98
sean-code <john.nganga@student.moringaschool.com>
2023-02-20 20:25:28 +0300

    Create seeds for the db using faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do
+    puts "Seeding Users.."
+    user1 = User.create(username: Faker::Twitter.screen_name, bio: Faker::Lorem.paragraph(sentence_count: 3), password: Faker::Config.random.seed )
+    user2 = User.create(username: Faker::Twitter.screen_name, bio: Faker::Lorem.paragraph(sentence_count: 3), password: Faker::Config.random.seed )
+    puts "Seeded Users!!"
+end
+10.times do
+    puts "Seeding Notes..."
+    note1 = Note.create(title: Faker::Book.title, content: Faker::Books::Lovecraft.sentence(word_count: 3), user: user1)
+    note2 = Note.create(title: Faker::Book.title, content: Faker::Books::Lovecraft.sentence(word_count: 2) user: user2)
+    puts "Seeded Notes!!"
+end
