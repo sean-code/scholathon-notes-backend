f477c88800a7c70cf0e74fd30e04162652716c44
sean-code <john.nganga@student.moringaschool.com>
2023-02-21 21:05:18 +0300

    Create actions for user controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,3 @@
+class NotesController < ApplicationController
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+    def index
+        @notes=Note.all
+        render json: @notes, except:[:created_at, :updated_at], status: :ok
+    end
+
+    def show
+        @note = Note.find_by(id: params[:id])
+        if hero
+            render json: @note, except: [:created_at, :updated_at], status: :ok
+        else
+            render json: {error: "Note Not Found"}, status: :not_found
+        end
+    end
+end
