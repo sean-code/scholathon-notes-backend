c06e388fe9019ed0e49d93270ac1af6e5ed1fc9b
sean-code <john.nganga@student.moringaschool.com>
2023-02-22 09:53:30 +0300

    Add CRUD actions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -12,4 +12,37 @@ class NotesController < ApplicationController
             render json: {error: "Note Not Found"}, status: :not_found
         end
     end
+
+    def update 
+        note = find_note
+        if note
+            note.update(note_params)
+            render json: note
+        else
+            render json: { error: "Note not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        note = find_note
+        if note
+            note.destroy
+            head :no_content
+        else
+            render json: { error: "note not found" }, status: :not_found
+        end
+    end
+
+    private
+    def find_note
+        Note.find_by(id: params[:id])
+    end
+
+    def note_params
+        params.permit(:title, :content)
+    end
+
+    def respond_record_not_found
+        render json: { error: "note not found" }, status: 404
+    end
 end
